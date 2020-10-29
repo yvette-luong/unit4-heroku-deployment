292570a27e7bdc87d5ea030820204959c954e24a
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-29 15:30:57 -0700

    create herokudepploy combine react app

---
+++ herokudeploy/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 3300
+const server = express()
+server.use(express.json())
+//serving our static assets
+server.use(express.static(path.join(__dirname, 'client/build')))
+//endpoint era
+server.get('/', (req, res) => {
+  res.json({ message: 'API is up!!!'})
+})
+//fallback endpoint that sends index.html
+server.get('*',(req, res) =>{
+    res.sendFile(paht.join(__dirname, 'client/build', 'index.html'))
+})
+
+server.listen(PORT, () => {
+  console.log(`**\nlistening on http://localhost:${PORT}\n**`)
+})
